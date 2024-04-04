cbb2a5df362c339dcc57978c1fa47bd8563104be
Upendra chary <77915639+Hack1434@users.noreply.github.com>
2024-04-04 22:01:43 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,12 +1,11 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBh9Lpx39WU5a7IGnnM9DYzUOfhK3R4rCs",
-  authDomain: "crypto-68974.firebaseapp.com",
-  projectId: "crypto-68974",
-  storageBucket: "crypto-68974.appspot.com",
-  messagingSenderId: "59651590688",
-  appId: "1:59651590688:web:f248d9b9bd692fa5104a45",
-  
+  apiKey: "AIzaSyBMfHUVXe4GDHryY7sJMPmnElS5kLDZZo4",
+  authDomain: "crypto-hunter-50991.firebaseapp.com",
+  projectId: "crypto-hunter-50991",
+  storageBucket: "crypto-hunter-50991.appspot.com",
+  messagingSenderId: "141181984273",
+  appId: "1:141181984273:web:ba51a25e27ce6a2b1d9bb0",
 };
 
 export default firebaseConfig;

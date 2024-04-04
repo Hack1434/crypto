88e9f48d4cf1927d8fb9e600607a60cce731a4e4
Hack1434 <uckbx001@gmail.com>
2024-04-04 21:38:59 +0530

    commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBh9Lpx39WU5a7IGnnM9DYzUOfhK3R4rCs",
+  authDomain: "crypto-68974.firebaseapp.com",
+  projectId: "crypto-68974",
+  storageBucket: "crypto-68974.appspot.com",
+  messagingSenderId: "59651590688",
+  appId: "1:59651590688:web:f248d9b9bd692fa5104a45",
+  
+};
+
+export default firebaseConfig;
